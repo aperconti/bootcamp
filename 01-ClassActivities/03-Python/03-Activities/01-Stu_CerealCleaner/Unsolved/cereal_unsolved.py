ad1e76e9a2652906d88b436f5c85f1b039f00d95
Autum Perconti <autumlucille@gmails>
2020-07-07 21:22:44 -0700

    adding txt file

--- 01-ClassActivities/03-Python/03-Activities/01-Stu_CerealCleaner/Unsolved/cereal_unsolved.py
+++ 01-ClassActivities/03-Python/03-Activities/01-Stu_CerealCleaner/Unsolved/cereal_unsolved.py
@@ -3,3 +3,17 @@ import csv
 
 cereal_csv = os.path.join("..", "Resources", "cereal.csv")
 
+
+with open(cereal_csv,'r') as cereal_data:
+    csvreader = csv.reader(cereal_data)
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(csv_header)
+
+    #first_row = next(csvreader)
+    #print(first_row)
+    for i in csvreader:
+            if float(i[7]) >= 5: 
+                print(i)
+
--- 01-ClassActivities/03-Python/03-Activities/03-Stu_HobbyBook/Unsolved/HobbyBook_Unsolved.py
+++ 01-ClassActivities/03-Python/03-Activities/03-Stu_HobbyBook/Unsolved/HobbyBook_Unsolved.py
@@ -1,1 +1,9 @@
 # @TODO: Your code here
+#creating dict 
+Mydict= {"name":"Autum",
+'age':29, 
+"Hobbies": ["skiing", "running", "mnt", "biking"], 
+'wake-up': {'mon':'7am', 'sat':'9am'}}
+
+print(f'Hello my name is {Mydict["name"]} and I am {Mydict["age"]}')
+print(f'I love to play and have {len(Mydict["Hobbies"])} hobbies')
