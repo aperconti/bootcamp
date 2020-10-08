dbd6f54a3114b3929de69ca1d769f74dd7ab2839
Autum Perconti <autumlucille@gmail.com>
2020-10-07 18:27:41 -0700

    pulling upstream master

--- 01-ClassActivities/15-Plotly/03-Activities/02-Stu_Switch/Unsolved/plots.js
+++ 01-ClassActivities/15-Plotly/03-Activities/02-Stu_Switch/Unsolved/plots.js
@@ -1,5 +1,17 @@
 // Initializes the page with a default plot
+function initilize() {
+    var trace = {
+        x: [1,2,3,4],
+        y: [10,15,20,25,]
+    };
 
+    var my_data = [trace];
+
+    var my_chart = d3.selectAll("#plot").node();
+
+    Plotly.newPlot(my_chart, my_data);
+
+};
 
 // Call updatePlotly() when a change takes place to the DOM
 
--- 01-ClassActivities/15-Plotly/03-Activities/02-Stu_Switch/Unsolved/switch.js
+++ 01-ClassActivities/15-Plotly/03-Activities/02-Stu_Switch/Unsolved/switch.js
@@ -1,1 +1,26 @@
 // Switch function
+function Converter(letter) {
+    switch (letter) {
+        case "a":
+            console.log(1);
+            break;
+        case "b":
+            console.log(2);
+            break;
+        case "c":
+            console.log(3);
+            break;
+        case "d":
+            console.log(4);
+            break;
+        case "e":
+            console.log(5);
+            break;
+        default:
+            console.log("please enter a letter A - E");
+            break;
+    }
+
+}
+
+Converter("d");
